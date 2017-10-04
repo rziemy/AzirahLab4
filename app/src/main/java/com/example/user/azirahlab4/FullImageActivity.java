295555587d82fd5c45ebb5d188136049b39c5786
Student5 <student5@iMac5s-iMac.local>
2017-10-04 13:21:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/azirahlab4/FullImageActivity.java
@@ -0,0 +1,26 @@
+package com.example.user.azirahlab4;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+
+
+public class FullImageActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstaceState) {
+        super.onCreate(savedInstaceState);
+        setContentView(R.layout.full_image);
+
+        Intent i = getIntent();
+
+        int position = i.getExtras().getInt("id");
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+
+        ImageView imageView = (ImageView) findViewById(R.id.full_image_view);
+        imageView.setImageResource(imageAdapter.mThumbIds[position]);
+    }
+}
+
---
+++ app/src/main/java/com/example/user/azirahlab4/ImageAdapter.java
@@ -0,0 +1,52 @@
+package com.example.user.azirahlab4;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public Integer[] mThumbIds = {
+            R.drawable.a, R.drawable.b,
+            R.drawable.c, R.drawable.d,
+            R.drawable.e, R.drawable.f
+
+
+    };
+
+    public ImageAdapter(Context c){
+        mContext = c;
+    }
+
+    @Override
+    public int getCount(){
+        return mThumbIds.length;
+    }
+
+    @Override
+    public Object getItem(int position){
+        return mThumbIds[position];
+    }
+
+    @Override
+    public long getItemId(int position){
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        ImageView imageView = new ImageView(mContext);
+        imageView.setImageResource(mThumbIds[position]);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setLayoutParams(new GridView.LayoutParams(150,150));
+        return imageView;
+    }
+}
+
+
